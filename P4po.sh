9b8acc7a0593f4494c82ada5df972733f0ed3dbd
P4PO-Gans <68926125+P4PO-Gans@users.noreply.github.com>
2020-07-29 03:59:02 +0700

    Create P4po.sh

---
+++ P4po.sh
@@ -0,0 +1,665 @@
+#!/bin/bash
+#version 1.0
+#Mr P4PO
+
+clear
+# Variable warna
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+  figlet -f slant "MR P4PO"|lolcat
+
+echo "     ============================="| lolcat
+echo "     [+] AUTHOR  : P4PO         [+]" | lolcat
+echo  "    [+] Concact : 08979398164  [+]" | lolcat
+echo "     [+] SUPPORT : ALLAH SWT    [+]" | lolcat
+echo "     ==============================" | lolcat
+
+sleep 1
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo $cyan"[#]>SEMOGA BERMANFAAT"           
+sleep 1
+echo ""
+echo $white"[#]>I HATE LOVE:("
+sleep 1
+exit
+}     
+        
+         echo "\33[1;36m SELAMAT DATANG"
+         echo "\33[32;1m TOOLS INI BERISI SCRIPT TERBARU 2020"
+         echo "\33[1;33m SEBELUM KALIAN MEMAKAI SCRIPT NYA"
+         echo "\33[1;33m KALIAN HARUS PILIH NOMER 99 "
+         echo "\33[1;33m UNTUK MENGINSTAL BAHAN-BAHAN NYA DLU"
+         
+
+         sleep 3
+         sleep 2
+
+echo "\33[32;1m SILAHKAN DI PILIH GANS"
+echo "\33[37;1m"
+      sleep 2
+      echo "\33[1;33m ( 1 )  HACK FB MOD-PRO"
+      echo "\33[32;1m ( 2 )  AUTO LIKE FB"
+      echo "\33[1;33m ( 3 )  MELACAK ORANG"
+      echo "\33[1;33m ( 4 )  CMATRIX"
+      echo "\33[32;1m ( 5 )  PHISING"
+      echo "\33[1;33m ( 6 )  CCTV"
+      echo "\33[32;1m ( 7 )  SPAM SMS 2020"
+      echo "\33[1;33m ( 8 )  KIRIM VIRUS" 
+      echo "\33[32;1m ( 9 )  VIRTEX GANAS WA" 
+      echo "\33[32;1m ( 10 ) DDOS TROJANS "
+      echo "\33[1;33m ( 11 ) HACK KAMERA"
+      echo "\33[1;33m ( 12 ) MEMBUAT SC DEFACE" 
+      echo "\33[32;1m ( 13 ) TEBAS INDEX"
+      echo "\33[1;33m ( 14 ) SPAM CALL 2020"
+      echo "\33[1;36m ( 15 ) SMS GRATIS" 
+      echo "\33[1;33m ( 16 ) SPAM SMS XL"
+      echo "\33[32;1m ( 17 ) SADAP WA"
+      echo "\33[1;36m ( 18 ) TOMBOL SPECIAL"
+      echo "\33[1;31m ( 19 ) INSTALL TOOLS TU4N B4DUT" 
+      echo "\33[1;36m ( 20 ) HACK FOOLOWERS"
+      echo "\33[1;31m ( 21 ) INSTALL TOLLS MRTAMFANX" 
+      echo "\33[1;36m ( 22 ) KUMPULAN WA KEBAL"
+      echo "\33[1;33m ( 23 ) RED HAWK"
+      echo "\33[32;1m ( 24 ) BUAT VIRUS GANAS"
+      echo "\33[1;36m ( 25 ) HACK FB TARGET"
+      echo "\33[1;33m ( 26 ) KATA-KATA BUCIN"
+      echo "\33[1;31m ( 27 ) HACK PULSA GRATIS"
+      echo "\33[32;1m ( 28 ) SPAMER BL4CK DR4GON"
+      echo "\33[1;36m ( 29 ) R-PASS"
+      echo "\33[1;36m ( 30 ) DUMP-FIG"
+      echo "\33[1;31m ( 31 ) FB DARK V16-PRO"
+      echo "\33[32;1m ( 32 ) BOT-CHAT"
+      echo "\33[1;36m ( 33 ) ENCRIPT SC PYTHON"
+      echo "\33[1;31m ( 34 ) FB DARK V17-PRO"
+      echo "\33[1;32m ( 35 ) HACK WIFI"
+      echo "\33[1;32m ( 36 ) A-RAT"
+      echo "\33[1;31m ( 37 ) HACK LISTRIK"
+      echo "\33[1;36m ( 38 ) OSIF"
+      echo "\33[1;32m ( 39 ) BOT REACTION FB"
+      echo "\33[32;1m ( 40 ) TEMBAK KUOTA GRATIS"
+      echo "\33[1;31m ( 41 ) SPAM WA"
+      echo "\33[1;36m ( 42 ) AUTO VISITOR"
+      echo "\33[1;33m ( 43 ) ADMIN FINDER"
+      echo "\33[1;33m ( 44 ) MIRORR ZONE-H"
+      echo "\33[1;32m ( 45 ) I-GENERATOR"
+      echo "\33[1;32m ( 46 ) SPAM WA 2020"
+      echo "\33[1;31m ( 47 ) DAIJOBU"
+      echo "\33[1;32m ( 48 ) FB GUARD PROFIL"
+      echo "\33[1;33m ( 49 ) KAWAI-BOTNET"      
+      echo "\33[1;32m ( 50 ) METASPLOIT"
+      echo "\33[1;33m ( 51 ) TOOLS RUS"
+      echo "\33[1;33m ( 52 ) TOOLS P4PO V2"
+      echo "\33[1;36m ( 00 ) EXIT"
+      echo "\33[1;33m ( 99 ) INSTALL BAHAN-BAHAN NYA"  
+      echo "---------------------------------------"
+read -p " MASUKAN PILIHAN ANDA : " pil ;
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+sleep 1
+git clone https://github.com/Mr-ARIYA/MOD-PRO
+cd MOD-PRO
+python2 Crack.py
+fi
+
+sleep 2
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+
+fi
+
+if [ $pil =  3 ]
+then
+ clear
+ figlet -f slant "Jawa Barat Cyber"|lolcat
+git clone https://github.com/kereh/Findip
+cd Findip
+    python2 findipv2.py
+fi
+
+if [ $pil = 4 ]
+then
+echo "SELAMAT MENIKMATI"
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+sleep 1
+gpit clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+sleep 1
+pkg install python2
+    pip2 install requests
+    git clone https://github.com/kancotdiq/ipcs
+    cd ipcs
+    python2 scan.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://https://github.com/Fukur0-3XP/SpamSms
+cd SpamSms
+ls
+python2 Spam.py
+
+
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber" |lolcat
+git clone https://github.com/novalattasya/6VIRUS
+cd 6VIRUS
+sh 6VIRUS.sh
+fi
+
+if [ $pil =9 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+sh virtex.sh
+
+fi
+
+if [ $pil =10 ]
+then
+clear
+git clone https://github.com/BPCATTACKER/alinetv2                        
+cd alinetv2                             
+sh alinetv2.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber" |lolcat
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber" |lolcat
+git clone https://github.com/Laskar-Muslim-Cyber/create-sc-deface.git
+cd create-sc-deface
+python2 create-sc.py
+
+fi
+
+if [ $pil = 13 ]
+then
+clear
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEFACE
+sh install.sh
+sh AOC.sh
+
+fi
+
+if [ $pil = 14 ]
+then
+clear
+git clone https://github.com/flyngdutchman/SpamCall
+cd SpamCall
+ls
+python SpamCall.py
+
+fi
+
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+git clone  https://github.com/widhisec/sms-gratis
+cd sms-gratis
+bash smsgratis.sh
+
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+git clone https://github.com/Rusmana-ID/spam-xl-unlimited
+cd spam-xl-unlimited
+bash spam-xl.sh
+
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber" |lolcat
+git clone https://github.com/RyuXploit/Gabutz
+cd Gabutz
+sh gbtz.sh
+
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber" |lolcat
+git clone https://github.com/MiSetya/tombol-special
+cd tombol-special
+php Aktif.php
+
+fi
+
+if [ $pil = 19 ]
+then
+clear
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sh TUANB4DUT.sh
+
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "Jawa Barat Cyber"|lolcat
+pkg install git
+pkg install nodejs
+git clone http://github.com/rrudy0966/instatools
+cd instatools
+unzip node_modules.zip
+pkg install nodejs lts
+npm install instagram-private-api
+node index
+
+fi
+
+
+if [ $pil = 21 ]
+then 
+clear
+git clone https://github.com/MrTamfanX/tollspam
+cd tollspam
+sh tollspam.sh
+
+fi
+
+
+if [ $pil = 22 ]
+then
+clear
+sleep 1
+figlet -f slant "Loading"|lolcat
+git clone https://github.com/Makky2693/WhatsApp-Downloader
+cd WhatsApp-Downloader
+sh wa.sh
+
+fi
+
+if [ $pil = 23 ]
+then
+clear
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+
+fi
+
+if [ $pil = 24 ]
+then
+clear
+git clone https://github.com/LOoLzeC/Evil-create-framework
+cd Evil-create-framework
+sh install.sh
+
+fi
+
+if [ $pil = 25 ]
+then
+clear
+git clone https://github.com/MRA27/FBNEWV2
+cd FBNEWV2
+python2 MRA27.py
+
+fi
+
+if [ $pil = 26 ]
+then
+clear
+git clone https://github.com/MiSetya/BUCIN
+cd BUCIN
+sh BUCIN.sh
+
+fi
+
+if [ $pil = 27 ]
+then
+clear
+git clone https://github.com/MiSetya/PulsaGratis
+cd PulsaGratis
+sh Gratis.sh
+
+fi
+
+if [ $pil = 28 ]
+then
+clear
+git clone https://github.com/Bl4ckDr460n/SPAMer
+cd SPAMer
+python2 SPAMer.py
+
+fi
+
+if [ $pil = 29 ]
+then
+clear
+git clone https://github.com/Bl4ckDr460n/RPass
+cd RPass
+python RPass.py
+
+fi
+
+if [ $pil = 30 ]
+then
+clear
+git clone https://github.com/Bl4ckDr460n/Dump-FIG
+cd Dump-FIG
+python dump.py
+
+fi
+
+if [ $pil = 31 ]
+then
+clear
+figlet -f slant "M4J4L3NGK4 CYB3R T3∆M"|lolcat
+git clone https://github.com/Mr-ARIYA/V16-PRO
+cd V16-PRO
+python2 Fbv6.py
+
+fi
+
+if [ $pil = 32 ]
+then
+clear
+git clone https://github.com/Bl4ckDr460n/BotChat
+cd BotChat
+python2 Chat.py
+
+fi
+
+if [ $pil = 33 ]
+then
+clear
+git clone https://github.com/Bl4ckDr460n/MarshBase
+cd MarshBase
+python2 MarshBase.py
+
+fi
+
+if [ $pil = 34 ]
+then
+clear
+git clone https://github.com/Mr-ARIYA/V17-PRO
+cd V17-PRO
+python2 Fbv17.py
+
+fi
+
+
+if [ $pil = 35 ]
+then
+clear
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+Chmod +×* wifi-hacker.sh
+ls
+./wifi-hacker.sh
+
+fi
+
+if [ $pil = 36 ]
+then
+clear
+git clone https://github.com/Xi4u7/A-
+Rat.git
+cd A-Rat
+python2 A-Rat.py
+
+fi
+
+if [ $pil = 37 ]
+then
+clear
+git clone 
+git clone https://github.com/LionSec/katoolin.git
+ls
+cd katoolin
+ls
+python2 katoolin.py
+nano katoolin.py
+
+fi
+
+if [ $pil = 38 ]
+then
+clear
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+python2 osif.py
+
+fi
+
+if [ $pil = 39 ]
+then
+clear
+git clone https://github.com/AMVengeance/FB-
+React.git
+cd FB-React
+./start
+
+fi
+
+if [ $pil = 40 ]
+then
+clear
+git clone https://github.com/MrTamfanX/TembakKuota
+cd TembakKuota
+sh IsiKuota.sh
+
+fi
+
+if [ $pil = 41 ]
+then
+clear
+git clone https://github.com/sandiwijayani1/SpamWa
+cd SpamWa
+python3 spam.py
+
+fi
+
+if [ $pil = 42 ]
+then
+clear
+git clone https://github.com/Fukur0-3XP/AutoVisitor
+cd AutoVisitor
+python2 Visitor.py
+
+fi
+
+if [ $pil = 43 ]
+then
+clear
+git clone https://github.com/Fukur0-3XP/AdminFinder
+cd AdminFinder
+python2 Finder.py
+
+fi
+
+if [ $pil = 44 ]
+then
+clear
+git clone https://github.com/Fukur0-3XP/ZoneH
+cd ZoneH
+python2 ZoneH.py
+
+fi
+
+if [ $pil = 45 ]
+then
+clear
+git clone https://github.com/Fukur0-3XP/I-Gen
+cd I-Gen
+python2 I-Gen.py
+
+fi
+
+if [ $pil = 46 ]
+then
+clear
+git clone https://github.com/sandiwijayani1/SpamWa
+cd SpamWa
+ls
+python spam.py
+
+fi
+
+if [ $pil = 47 ]
+then
+clear
+git clone https://github.com/alintamvanz/diejoubu
+cd diejoubu
+cd v1.2
+php diejubu.php
+
+fi
+
+if [ $pil = 48 ]
+then
+clear
+git clone clone https://github.com/Al2VyN/FB-Guard.git
+cd FB-Guard
+php guard.php
+
+fi
+
+if [ $pil = 49 ]
+then
+clear
+git clone https://github.com/DedSecCyber/Kawai-Botnet
+cd Kawai-Botnet
+ls
+php Kawai.php
+
+ls
+
+if [ $pil = 50 ]
+then
+clear
+curl -LO 
+https://raw.githubusercontent.com/Hax4
+us/Metasploit_termux/master/metasploit
+.sh
+chmod +x metasploit.sh
+./metasploit.sh
+
+fi
+
+if [ $pil = 51 ]
+then
+clear
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+if [ $pil = 52 ]
+then
+clear
+git clone https://github.com/P4PO/Tools
+cd Tools
+sh p4po.sh
+fi
+
+
+if [ $pil = 99 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+apt install curl
+apt install grep
+apt install bc
+pkg install nodejs
+apt install ncurses-utils pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " DONE"|lolcat
+
+fi
+
+if [ $fil = 00 ]
+
+then
+clear
+echo "MOHON MAAFF JIKA ADA KESALAHAN"  
+      sleep
+echo  $Keluar
+
+fi
+
+                                                                  
+else
+       echo "Terjadi Masalah Eror"
+            sleep
+       echo $ulang
+fi
